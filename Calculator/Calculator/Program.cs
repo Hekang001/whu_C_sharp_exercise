9f2e2e41880438f4177baa7f79ca15f99db5dad6
Administrator <Administrator@HEKANG>
2020-02-22 20:41:53 +0800

    Calculator & Calculator_windows

---
+++ Calculator/Calculator/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //输入要运算的数字和运算符
+            Console.WriteLine("请输入数字A");
+            double A = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("请输入运算符");
+            string operate = Console.ReadLine();
+            Console.WriteLine("请输入数字B");
+            double B = Convert.ToDouble(Console.ReadLine());
+            //将结果存储到变量D里面
+            double D = 0;
+            //用if条件语句判断输入的符号而进行相应的运算并输出
+            if (operate == "+")
+            {
+                D = A + B;
+                Console.WriteLine("A+B=" + D);
+            }
+            else if (operate == "-")
+            {
+                D = A - B;
+                Console.WriteLine("A-B=" + D);
+            }
+            else if (operate == "*")
+            {
+                D = A * B;
+                Console.WriteLine("A*B=" + D);
+            }
+            else if (operate == "/")
+            {
+                if (B == 0)
+                {
+                    Console.WriteLine("不能除零操作！");
+                }
+                else
+                {
+                    D = A / B;
+                    Console.WriteLine("A/B=" + D);
+                }
+
+            }
+            else if (operate == "")
+            {
+                Console.WriteLine("The operator is null");
+            }
+            else
+            {
+                Console.WriteLine("The operator is illegal");
+            }
+               
+        }
+    }
+}
---
+++ Calculator_window/Calculator_window/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator_window
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "=";
+            button1.Text = "选择不同运算符进行计算";
+            comboBox1.Items.Add("+");
+            comboBox1.Items.Add("-");
+            comboBox1.Items.Add("*");
+            comboBox1.Items.Add("/");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float a, b, c;
+            a = Convert.ToSingle(textBox1.Text);
+            b = Convert.ToSingle(textBox2.Text);
+            if (comboBox1.Text == "+")
+            {
+                c = a + b;
+                textBox3.Text = c.ToString();
+            }
+            else if (comboBox1.Text == "-")
+            {
+                c = a - b;
+                textBox3.Text = c.ToString();
+            }
+            else if (comboBox1.Text == "*")
+            {
+                c = a * b;
+                textBox3.Text = c.ToString();
+
+            }
+            else 
+            {
+                if (b == 0)
+                {
+                    textBox3.Text = "被除数不能为0";
+                }
+                else
+                {
+                    c = a / b;
+                    textBox3.Text = c.ToString();
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
